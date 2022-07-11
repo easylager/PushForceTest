1e160a0b809150de7d63be7436bea8498ec00ad3
Maksim <oceandrinkparadise@gmail.com>
2022-07-11 23:11:27 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+list_testing = list((2, 3, 5, 5))
+print(list_testing)
+pop_one = list_testing.pop(0)
+print(list_testing)
+print(f'pop_one:{pop_one}')
+print(type(item for item in list_testing))
+
+generator_testing = (item for item in list_testing)
+def generator_testing_function():
+    n = 1
+    yield n
+
+    n += 1
+    yield n
+
+    n += 1
+    yield n
+
+
+if __name__ == "__main__":
+    gen_func = generator_testing_function()
+    a = next(gen_func)
+    print(a)
+    print(next(gen_func))
+    print(next(gen_func))
