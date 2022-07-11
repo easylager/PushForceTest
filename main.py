7b41125d9c06ae3d4d92de8ab7444d718b3a65b2
Maksim <oceandrinkparadise@gmail.com>
2022-07-11 23:28:24 +0300

    sodfis

--- main.py
+++ main.py
@@ -1,10 +1,1 @@
-list_testing = list((2, 3, 5, 5))
-print(list_testing)
-pop_one = list_testing.pop(0)
-print(list_testing)
-print(f'pop_one:{pop_one}')
-print(type(item for item in list_testing))
 
-generator_testing = (item for item in list_testing)
-
-git = lambda a: a+1
