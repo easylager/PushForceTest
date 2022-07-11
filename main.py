d5a0978a76d7f5ce586afb6d033c22b961c46b2f
Maksim <oceandrinkparadise@gmail.com>
2022-07-12 00:21:13 +0300

    sdiofjdsijfidfjosdifjsdoifjsd

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
+list_testing = list((2, 3, 5, 5))
 print(list_testing)
 pop_one = list_testing.pop(0)
-print(list_testing)
-print(f'pop_one:{pop_one}')
-print(type(item for item in list_testing))
+
 
 generator_testing = (item for item in list_testing)
 
-git = lambda a: a+1
-sdklfnsdklfnsdklfnksldf
+if __name__ == "__main__":
+    gen_func = generator_testing_function()
+
+    sdifjsfjsdfiosdjfiosdjf
+
+    list_testing = list((2, 3, 5, 5))
+    print(list_testing)
+    pop_one = list_testing.pop(0)
+
+    generator_testing = (item for item in list_testing)
+
+    if __name__ == "__main__":
+        gen_func = generator_testing_function()
+
+        sdifjsfjsdfiosdjfiosdjf
+
+        list_testing = list((2, 3, 5, 5))
+        print(list_testing)
+        pop_one = list_testing.pop(0)
+
+        generator_testing = (item for item in list_testing)
+
+        if __name__ == "__main__":
+            gen_func = generator_testing_function()
+
+            sdifjsfjsdfiosdjfiosdjf
