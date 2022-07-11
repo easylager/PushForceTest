bfa41cb21cbf13a27a9846fa1b43f14a0d6ac04e
Maksim <oceandrinkparadise@gmail.com>
2022-07-11 23:32:31 +0300

    dsfsf

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+print(list_testing)
+pop_one = list_testing.pop(0)
+print(list_testing)
+print(f'pop_one:{pop_one}')
+print(type(item for item in list_testing))
 
+generator_testing = (item for item in list_testing)
+
+git = lambda a: a+1
